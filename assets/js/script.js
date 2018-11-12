888de713eade28626a02a2a72253194e4344fce4
erandro <erandro@gmail.com>
2018-11-11 19:41:41 -0600

    added cursor:pointer

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,14 +52,29 @@ $(".eran-img-3").click(function () {
         scrollTop: $("#contact").offset().top
     }, 1000);
 });
+$("#text-box-1").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top
+    }, 1000);
+});
 $(".eran-img-5").click(function () {
     $('html, body').animate({
         scrollTop: $("#projects").offset().top
     }, 1000);
 });
+$("#text-box-2").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#skills").offset().top
+    }, 1000);
+});
 $(".eran-img-6").click(function () {
     $('html, body').animate({
         scrollTop: $("#skills").offset().top
+    }, 1000);
+});
+$("#text-box-3").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
     }, 1000);
 });
 $(".eran-img-7").click(function () {
